d3cb5103e3d31c2f6ad7dfb9543bd16db74cbc34
David <dcamalv@gmail.com>
2021-07-04 18:49:13 +0200

    .

--- src/main/java/com/davcamalv/filmApp/services/JustWatchService.java
+++ src/main/java/com/davcamalv/filmApp/services/JustWatchService.java
@@ -2,6 +2,7 @@ package com.davcamalv.filmApp.services;
 
 import javax.transaction.Transactional;
 
+import org.jboss.logging.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,22 +11,26 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 
+import com.davcamalv.filmApp.controllers.PruebasController;
+
 @Service
 @Transactional
 public class JustWatchService {
 
+	protected final Logger log = Logger.getLogger(JustWatchService.class);
+
 	public void getPremieres() {
+		ChromeOptions options = new ChromeOptions();
+		options.setHeadless(true);
+		WebDriver webDriver = new ChromeDriver(options);
 		try {
-			ChromeOptions options = new ChromeOptions();
-			options.setHeadless(false);
-			WebDriver webDriver = new ChromeDriver(options);
 			WebDriverWait wait = new WebDriverWait(webDriver, 15);
 			webDriver.get("https://www.justwatch.com/es/nuevo");
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("timeline")));
 		} catch (Exception e) {
-			System.out.println("Selenium error");
+			log.error("selenium error", e);
 		} finally {
-			
+			webDriver.close();
 		}
 	}
 }
